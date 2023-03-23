293e4f471979be2bde923c0d3a501a5a165037c5
KimonoBoy <simo17k5@easv365.dk>
2023-03-23 16:30:58 +0100

    Updated class name

---
+++ scripts/NucleiLite/NucleiLite/Main.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+using GTA;
+using GTA.UI;
+
+namespace NucleiLite
+{
+    public class Main : Script
+    {
+        public Main()
+        {
+            KeyDown += OnKeyDown;
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.T) Notification.Show("Hello Compiled Script!");
+        }
+    }
+}
