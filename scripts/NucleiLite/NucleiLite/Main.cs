4b5d02ede4593c0182e7e59bccdf52a8cf2114b2
KimonoBoy <simo17k5@easv365.dk>
2023-03-25 18:41:08 +0100

    Fixed NucleiLite Mod

--- scripts/NucleiLite/NucleiLite/Main.cs
+++ scripts/NucleiLite/NucleiLite/Main.cs
@@ -15,21 +15,14 @@ namespace NucleiLite
         NativeMenu vehicleSpawnerMenu = new NativeMenu("NucleiLite", "Vehicle Spawner Menu");
         NativeMenu weaponsMenu = new NativeMenu("NucleiLite", "Weapons Menu");
 
-        bool CanSuperJump;
-
         public Main()
         {
-            // Main Menu
             CreateMainMenu();
-
-            // Player Menu
             CreatePlayerMenu();
-
-            // Vehicle Spawner Menu
             CreateVehicleSpawnerMenu();
-
-            // Weapons Menu
             CreateWeaponsMenu();
+
+            AddMenusToPool();
 
             KeyDown += OnKeyDown;
             Tick += OnTick;
@@ -37,57 +30,16 @@ namespace NucleiLite
 
         private void CreateMainMenu()
         {
-            menuPool.Add(mainMenu);
             mainMenu.AddSubMenu(playerMenu);
             mainMenu.AddSubMenu(vehicleSpawnerMenu);
             mainMenu.AddSubMenu(weaponsMenu);
         }
 
-        private void CreateWeaponsMenu()
-        {
-            menuPool.Add(weaponsMenu);
-            var itemGiveAllWeapons = new NativeItem("Give All Weapons", "Gives all Weapons to the Player");
-            itemGiveAllWeapons.Activated += (sender, args) =>
-            {
-                foreach (WeaponHash weaponHash in Enum.GetValues(typeof(WeaponHash)))
-                {
-                    Game.Player.Character.Weapons.Give(weaponHash, 10, true, true);
-                    Game.Player.Character.Weapons[weaponHash].Ammo = Game.Player.Character.Weapons[weaponHash].MaxAmmo;
-                    Game.Player.Character.Weapons[weaponHash].AmmoInClip = Game.Player.Character.Weapons[weaponHash].MaxAmmoInClip;
-                }
-                Notification.Show("All Weapons Given.");
-            };
-            weaponsMenu.Add(itemGiveAllWeapons);
-        }
-
-        private void CreateVehicleSpawnerMenu()
-        {
-            menuPool.Add(vehicleSpawnerMenu);
-            foreach (VehicleHash vehicleHash in Enum.GetValues(typeof(VehicleHash)))
-            {
-                var itemVehicle = new NativeItem(vehicleHash.ToString(), $"Spawn the {vehicleHash.ToString()}");
-                itemVehicle.Activated += (sender, args) => 
-                {
-                    var vehicleModel = new Model(vehicleHash);
-                    vehicleModel.Request();
-
-                    var vehicle = World.CreateVehicle(vehicleModel, Game.Player.Character.Position + Game.Player.Character.ForwardVector * 3.0f, Game.Player.Character.Heading + 90.0f);
-
-                    vehicleModel.MarkAsNoLongerNeeded();
-
-                    Notification.Show($"Vehicle: {vehicleHash.ToString()} Spawned!");
-                };
-                vehicleSpawnerMenu.Add(itemVehicle);
-            }
-        }
-
         private void CreatePlayerMenu()
         {
-            menuPool.Add(playerMenu);
-
             // Fix Player
-            var itemFixPlayer = new NativeItem("Fix Player");
-            itemFixPlayer.Activated += (sender, args) => 
+            NativeItem itemFixPlayer = new NativeItem("Fix Player", "Restores Player's Health and Armor");
+            itemFixPlayer.Activated += (sender, args) =>
             {
                 Game.Player.Character.Health = Game.Player.Character.MaxHealth;
                 Game.Player.Character.Armor = Game.Player.MaxArmor;
@@ -96,49 +48,49 @@ namespace NucleiLite
             playerMenu.Add(itemFixPlayer);
 
             // Invincible
-            var checkBoxInvincible = new NativeCheckboxItem("Invincible", "Set The Player Invincible", Game.Player.Character.IsInvincible);
-            checkBoxInvincible.CheckboxChanged += (sender, args) => 
+            NativeCheckboxItem checkBoxInvincible = new NativeCheckboxItem("Invincible", "Your character can no longer die.");
+            checkBoxInvincible.CheckboxChanged += (sender, args) =>
             {
-                Game.Player.Character.IsInvincible = !Game.Player.Character.IsInvincible;
-                Notification.Show($"Player Invincible: {Game.Player.Character.IsInvincible}");
+                Game.Player.Character.IsInvincible = checkBoxInvincible.Checked;
+                Notification.Show($"Invincible: {Game.Player.Character.IsInvincible}");
             };
             playerMenu.Add(checkBoxInvincible);
 
-            // Change Wanted Level
-            var listItemWantedLevel = new NativeListItem<int>("Wanted Level", "Sets the Player's Wanted Level", 0, 1, 2, 3, 4, 5);
-            listItemWantedLevel.ItemChanged += (sender, args) => 
+            // Wanted Level
+            NativeListItem<int> listItemWantedLevel = new NativeListItem<int>("Wanted Level", "Adjust Player's Wanted Level.", 0, 1, 2, 3, 4, 5);
+            listItemWantedLevel.ItemChanged += (sender, args) =>
             {
                 Game.Player.WantedLevel = args.Object;
             };
-            listItemWantedLevel.SelectedItem = Game.Player.WantedLevel;
             playerMenu.Add(listItemWantedLevel);
+        }
 
-            // Super Jump
-            var checkBoxSuperJump = new NativeCheckboxItem("Super Jump", "Allows the Player to jump higher than a building!", CanSuperJump);
-            checkBoxSuperJump.CheckboxChanged += (sender, args) => 
-            { 
-                CanSuperJump = !CanSuperJump;
-                Notification.Show($"Super Jump Enabled: {CanSuperJump}");
-            };
-            playerMenu.Add(checkBoxSuperJump);
+        private void CreateWeaponsMenu()
+        {
+        }
+
+        private void CreateVehicleSpawnerMenu()
+        {
+        }
+
+        private void AddMenusToPool()
+        {
+            menuPool.Add(mainMenu);
+            menuPool.Add(playerMenu);
+            menuPool.Add(vehicleSpawnerMenu);
+            menuPool.Add(weaponsMenu);
         }
 
         private void OnTick(object sender, EventArgs e)
         {
             menuPool.Process();
-
-            if (CanSuperJump)
-            {
-                Game.Player.SetSuperJumpThisFrame();
-            }
         }
 
         private void OnKeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.F5)
             {
-                mainMenu.Visible = !mainMenu.Visible;
-                
+                mainMenu.Visible = !mainMenu.Visible;                
             }
         }
     }
